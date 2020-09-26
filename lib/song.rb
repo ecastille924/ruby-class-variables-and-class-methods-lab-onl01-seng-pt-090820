7e257134b7bf82c2aa7ab9ce602edd9231b97c9f
Erick Castille <ecastille924@gmail.com>
2020-09-26 19:24:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,21 @@ class Song
   
   @@song_count = 0 
   
-  def initialize
+  @@genres = []
+  @@artists = []
+  
+  def initialize (name, artist, genre)
     @@song_count =+ 1 
+    @@name = name
+    @@artist = artist
+    @@genre = genre 
   end
   
   def self.count 
     @@song_count
   end
   
+  def self.genres
+    @@genres.
+  end
 end
