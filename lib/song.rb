8bbf7660f676412078e2276f2bfef6ec0c540243
Erick Castille <ecastille924@gmail.com>
2020-09-26 19:36:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,13 @@ class Song
   def self.artist_count
     artist_count = {}
     @@arist_count.each do |artist|
-      if artist_count[artist]
+      if artist_count[artist] += 1
+        artist_count[artist]
+      else 
+        artist_count[arist] = 1 
+      end
+      artist_count
+    end
   
   def self.genre_count
     genre_count = {}
